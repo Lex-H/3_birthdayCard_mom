50eab01a669662bfb812129546b9045673076165
Lex-H <ckfly4014737@gmail.com>
2024-07-14 17:10:18 +0800

    新建檔案，做到接近完成，部屬到手機上看看

---
+++ index.js
@@ -0,0 +1,21 @@
+// 輪播圖
+$(document).ready(function(){
+    $('.carousel').slick({
+        // 自動播放
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+
+
+    });
+});
+
+// 批次展示照片
+var countI = 0;
+let picTemple = "";
+for (i=1;i<=39;i++) {
+    picTemple = picTemple + '<img class="pic" src="img/pic/' + i + '.jpg"></img>'
+    countI =+1;
+}
+document.querySelector(".picContainer").innerHTML = picTemple;
